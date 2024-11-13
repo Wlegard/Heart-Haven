586f1ff1c988c45f4eebc023d7c8ba4f34ed789b
whitley <whitleylegard.com>
2024-11-13 16:29:23 -0600

    done

--- index.js
+++ index.js
@@ -2,15 +2,32 @@
 $(document).ready(() => {
   const $body = $('body');
   $body.html(''); //clear the body (.html('')clears whatever tag)
+  const $tweetHolder = $('<div class="tweets-holder"></div>'); //
+  $body.append($tweetHolder);  // sweetholder add at the end of the body
+
+
+  const timeStamp = function(){}
 
   const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
+    const $tweet = $('<div class="tweet"></div>'); // set class to "tweet"
     const text = `@${tweet.user}: ${tweet.message}`;
 
     $tweet.text(text);
 
     return $tweet;
   });
-  $body.append($tweets);
+  $tweetHolder.append($tweets);//
+
+  const newTweet = function(tweet){ //
+    const $tweet = $('<div class="tweet"></div>'); //
+    const text = `@${tweet.user}: ${tweet.message}`; //
+    $tweet.text(text);//
+    $tweetHolder.prepend($tweet)//
+  }
+
+
+
+
+
 
 });
